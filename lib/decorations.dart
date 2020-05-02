7e772dc10ef638abebe86dec246279e47b583daa
Kishore S Shenoy <kichappa@gmail.com>
2020-05-02 22:51:02 +0530

    Added Button and animation

--- lib/decorations.dart
+++ lib/decorations.dart
@@ -1,7 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:test1/concave_decorations.dart';
 
-var inputShadow = BoxDecoration(
+var buttonDownOuter = BoxDecoration(
+    boxShadow: [
+      BoxShadow(
+        color: Color.fromRGBO(55, 84, 170, 0.1),
+        blurRadius: 8, // has the effect of softening the shadow
+        offset: Offset(
+          6, // horizontal, move right 10
+          6, // vertical, move down 10
+        ),
+      ),
+      BoxShadow(
+        color: Color.fromRGBO(255, 255, 255, 0.69),
+        blurRadius: 10, // has the effect of softening the shadow
+        offset: Offset(
+          -4, // horizontal, move right 10
+          -4, // vertical, move down 10
+        ),
+      ),
+    ],
+    borderRadius: BorderRadius.circular(26),
+    gradient: LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [const Color(0xFFDDE4ED), const Color(0xFFD3DBE4)]));
+
+var buttonDownInner = ConcaveDecoration(
+  shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(26),
+  ),
+  colors: [
+    Color.fromRGBO(106, 149, 212, 0.18),
+    Color.fromRGBO(232, 241, 252, 0.61)
+  ],
+  depression: 10,
+);
+
+var baseTileUpIner = ConcaveDecoration(
+  shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(26),
+  ),
+  colors: [
+    Color.fromRGBO(255, 255, 255, 0.64),
+    Color.fromRGBO(166, 169, 177, 0.95)
+  ],
+  depression: 1,
+);
+var baseTileUpOuter = BoxDecoration(
     boxShadow: [
       BoxShadow(
         color: Color.fromRGBO(43, 58, 103, 0.21),
@@ -26,15 +72,15 @@ var inputShadow = BoxDecoration(
         end: Alignment.bottomRight,
         colors: [const Color(0xFFE0E7F0), const Color(0xFFD2D9E3)]));
 
-var inputInnerShadow = ConcaveDecoration(
+var inputInner = ConcaveDecoration(
   shape: RoundedRectangleBorder(
     borderRadius: BorderRadius.circular(26),
   ),
   colors: [
-    Color.fromRGBO(41, 87, 121, 0.30),
+    Color.fromRGBO(41, 87, 121, 0.26),
     Color.fromRGBO(241, 244, 254, 0.75)
   ],
-  depression: 7,
+  depression: 10,
 );
 
 TextStyle inputText(double fontSize) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:test1/concave_decorations.dart';
+// import 'package:test1/concave_decorations.dart';
 
 import 'package:test1/decorations.dart';
 
@@ -9,24 +9,128 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Insti Super App',
-        home: Container(
-          decoration: baseInputDown,
-          child: Scaffold(
-            backgroundColor: Colors.transparent,
-            body: Center(
-                child: Container(
-                    child: Column(
-              mainAxisSize: MainAxisSize.max,
+    return MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage();
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: baseInputDown,
+      child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Stack(children: [
+            LoginInputContainer(),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              child: LoginButtons(),
+            ),
+          ])),
+    );
+  }
+}
+
+class LoginButtons extends StatelessWidget {
+  LoginButtons();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.only(right: 30, bottom: 43),
+        child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[
+        LoginButton(title: "Use\nGoogle"),
+        SizedBox(width:15), 
+        LoginButton(title: "Login"),
+      ],
+    ));
+  }
+}
+
+class LoginButton extends StatefulWidget {
+  final String title;
+  final bool isChosen;
+  // final VoidCallback onPressed;
+  LoginButton({
+    Key key,
+    @required this.title,
+    this.isChosen = false,
+    // @required this.onPressed,
+  }) : super(key: key);
+  @override
+  _LoginButtonState createState() => _LoginButtonState();
+}
+
+class _LoginButtonState extends State<LoginButton> {
+  bool _isPressed = false;
+  var screenWidth, screenHeight;
+
+  void _onPointerDown(PointerDownEvent event) {
+    setState(() => _isPressed = true);
+    // widget.onPressed();
+  }
+
+  void _onPointerUp(PointerUpEvent event) {
+    setState(() => _isPressed = widget.isChosen);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        alignment: Alignment.bottomCenter,
+        // decoration: BoxDecoration(color: Colors.blue),
+        child: Listener(
+            onPointerDown: _onPointerDown,
+            onPointerUp: _onPointerUp,
+            child: AnimatedContainer(
+              width: MediaQuery.of(context).size.width * 120 / 375,
+              height: MediaQuery.of(context).size.height * 143 / 812,
+              duration: const Duration(milliseconds: 300),
+              decoration: _isPressed ? buttonDownOuter : baseTileUpOuter,
+              child: Container(
+                  decoration: _isPressed ? buttonDownInner : baseTileUpIner,
+                  child: Stack(
+                    children: <Widget>[
+                      Positioned(
+                        left: _isPressed ? 21 : 20,
+                        bottom: _isPressed ? 19 : 20,
+                        child: Text(
+                          widget.title,
+                          style: inputText(20),
+                        ),
+                      )
+                    ],
+                  )),
+            )));
+  }
+}
+
+class LoginInputContainer extends StatelessWidget {
+  LoginInputContainer();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        // decoration: BoxDecoration(color: Colors.blue),
+        child: Align(
+            alignment: Alignment.center,
+            child: Container(
+                // decoration: BoxDecoration(color: Colors.red),
+                child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
               crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                    width: 265,
-                    decoration: BoxDecoration(
-                      color: Colors.transparent,
-                    ),
+              children: <Widget>[
+                FractionallySizedBox(
+                    widthFactor: 265 / 375,
                     child: Text(
                       "email",
                       style: inputText(15),
@@ -34,11 +138,8 @@ class MyApp extends StatelessWidget {
                 SizedBox(height: 5),
                 InputTextField(hintText: "kichappa@gmail.com"),
                 SizedBox(height: 3),
-                Container(
-                    width: 265,
-                    decoration: BoxDecoration(
-                      color: Colors.transparent,
-                    ),
+                FractionallySizedBox(
+                    widthFactor: 265 / 375,
                     child: Text(
                       "password",
                       style: inputText(15),
@@ -46,9 +147,7 @@ class MyApp extends StatelessWidget {
                 SizedBox(height: 5),
                 InputTextField(hintText: "••••••••")
               ],
-            ))),
-          ),
-        ));
+            ))));
   }
 }
 
@@ -59,29 +158,30 @@ class InputTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        width: 309,
-        height: 63,
-        decoration: inputShadow,
-        child: Center(
-            child: Container(
-                width: 309,
-                height: 60,
-                decoration: inputInnerShadow,
-                child: Center(
-                    child: Container(
-                        decoration: BoxDecoration(
-                          color: Colors.transparent,
-                        ),
-                        width: 265,
-                        child: TextField(
-                          decoration: InputDecoration(
-                            hintText: hintText,
-                            border: InputBorder.none,
-                          ),
-                          style: inputText(18),
-                          cursorColor: Color(0xff6F6F6F),
-                          cursorWidth: 3,
-                        ))))));
+    return FractionallySizedBox(
+        widthFactor: 309 / 375,
+        // heightFactor: 0.3,
+        child: Container(
+            // width: 39,
+            height: 60,
+            decoration: baseTileUpOuter,
+            child: Center(
+                child: Container(
+                    decoration: inputInner,
+                    child: Center(
+                        child: Container(
+                            decoration: BoxDecoration(
+                                // color: Colors.red,
+                                ),
+                            // width: 265,
+                            child: TextField(
+                              decoration: InputDecoration(
+                                  hintText: hintText,
+                                  border: InputBorder.none,
+                                  contentPadding: EdgeInsets.all(20.0)),
+                              style: inputText(18),
+                              cursorColor: Color(0xff6F6F6F),
+                              cursorWidth: 3,
+                            )))))));
   }
 }
